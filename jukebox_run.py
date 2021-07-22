687e3c1be3891419283bafcb9d3f1c40e5ee8693
veronmmason@gmail.com <veronmmason@gmail.com>
2021-07-21 22:31:20 -0400

    Created repo and added files for program to run. Initial commit for project.

---
+++ jukebox_run.py
@@ -0,0 +1,28 @@
+
+print("Thank you for using the best jukebox in the world, the Golden Standard!\nIt doesn't even play music!")
+
+user_name = input("What's your name?  ")
+
+
+def foo():
+    start_loop = input("{}, Would you like to build a playlist, select a premade playlist, or quit? Please enter 'build', 'premade', or 'quit'.  ".format(user_name))
+
+    if start_loop.upper() != "QUIT":
+        print("Thank you for using The Gold Standard")
+    elif start_loop.upper() == "BUILD":
+        #Build another loop starting on this line
+    elif start_loop.upper() == "PREMADE":
+        print("These playlists are")
+    #else for when the entered value is not one of the correct values
+
+
+
+round((x1+y1+z1)/60),0)
+    
+#class SongLibrary:
+    # def __init__(self, artist, album, tracklist):
+    #     self.artist = artist
+    #     self.album = album
+    #     self.tracklist = tracklist
+
+    # def  
