50262474576b0ecf400aa5878d7b373e2a4ba983
veronmmason@gmail.com <veronmmason@gmail.com>
2021-07-28 17:27:20 -0400

    Updated the README with details of the program and edited the program further to maximise usability.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# gold-standard-py
+#The Gold Standard, a Curated Jukebox
+
+##Description
+
+This is a simple CLI input program with a curated library of 34 albums.
+It functions similarly to a real life jukebox, with options for selecting premade playlists as well.
+
+##Features
+
+The album library is a JSON file that is stored in a variable and accessed when the loop starts to allow you to choose your songs. 
+You enter the amount of songs you would like to hear, then choose from the library, and the program captures your selection to work similarly to "credits." 
+After you've finished selecting, the program prints out your selection and its length by converting the sum of the seconds of each song to hh:mm:ss. 
+If you would like to see the premade playlists, the program will print them out when entering the option from the menu.
+
+
+##Installation
+Install the repo into the directory of your choosing, and run it in your machine's terminal. The program doesn't have any dependencies and is easily ran directly from the terminal.
--- jukebox_run.py
+++ jukebox_run.py
@@ -1,12 +1,14 @@
 
 import json
 
-with open ('jukebox_list.json', 'r', encoding="utf-8") as data_file:
+with open ('jukebox_list.json', 'r', encoding="utf-8") as data_file, open('premade_playlists.json', 'r', encoding="utf-8") as data_file2:
     album_library = json.load(data_file)
+    premade_playlists = json.load(data_file2)
 
 print("Thank you for using the best jukebox in the world, the Gold Standard!\nIt doesn't even play music!")
 
 user_name = input("What's your name?  ")
+
 
 def playlist_length(seconds):
     seconds = seconds % (24 * 3600)
@@ -17,33 +19,48 @@ def playlist_length(seconds):
       
     return "%d:%02d:%02d" % (hour, minutes, seconds)
 
+
 def jukebox():
     start_loop = input("{}, Would you like to build a playlist, select a premade playlist, or quit? Please enter 'build', 'premade', or 'quit'.  ".format(user_name))
 
     user_playlist = []
     list_run_time = []
-    
-    while start_loop.upper() == "QUIT":
-        print("Thank you for using The Gold Standard!")
-    
+
+
     if start_loop.upper() == "BUILD":
         requested_song_amount = int(input("Please enter the number of songs you would like to hear as a numeric digit.  "))
 
         for album in album_library:
-            print("{} {} by {}".format(album_library.index(album) + 1, album['album'], album['artist']))
+            print("{} {} by {}".format(album_library.index(album)+1, album['album'], album['artist']))
         for i in range(requested_song_amount):
             choose_album = int(input("Please enter the value of the album you would like to access.  "))
             [print("{} {}".format(album_library[choose_album-1]['tracklist'].index(x)+1,x), end='\n') for x in album_library[choose_album-1]['tracklist']]
             song_choice = int(input("Enter the numeric value of your song choice.  "))-1
             user_playlist.append(song_choice)
             list_run_time.append(album_library[choose_album-1]['tracklength'][song_choice])
-        print_playlist = user_playlist
-        print_playlist = ','.join(map(str, print_playlist)) 
-        print(print_playlist)
+            #this isn't printing the value of the index, it's printing the index itself
+        user_playlist = ','.join(map(str, user_playlist)) 
+        print(user_playlist)
         print(playlist_length(sum(list_run_time)))
 
     elif start_loop.upper() == "PREMADE":
-        print("Please choose a premade playlist.")
+        # choose_premade = int(input("Please enter the number of the playlist you'd like to select.\n"))
+        print()
+
+        premade1 = premade_playlists[0]['premade_pl']
+        premade1 = '\n'.join(premade1)
+        print(premade1)
+
+        premade2 = premade_playlists[1]['premade_pl']
+        premade2 = '\n'.join(premade2)
+        print(premade2)
+
+        premade3 = premade_playlists[2]['premade_pl']
+        premade3 = '\n'.join(premade3)
+        print(premade3)
+
+    elif start_loop.upper() != "QUIT":
+        print("Thank you for using The Gold Standard!")
 
     else:
         print("Please enter 'build', 'premade', or 'quit'.")
